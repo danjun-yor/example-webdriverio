314976473b0797c21d26188078029cb5e7d90665
Robert DeLuca <robertdeluca19@gmail.com>
2019-03-12 14:25:02 -0500

    Update to webdriverio 5 & break Percy
    Uncomment the percy config to see the test suite fail

--- specs/example-test.js
+++ specs/example-test.js
@@ -1,9 +1,10 @@
 var assert = require('assert');
 
 describe('example page', function() {
-    it('should look nice', function () {
-      browser.url('http://localhost:4567/')
-      browser.percySnapshot('sample');
-      assert.equal(browser.getTitle(), 'jQuery • TodoMVC');
-    });
+  it('should look nice', function () {
+    browser.url('http://localhost:8000/')
+    $('#new-todo').waitForExist(5000);
+    // browser.percySnapshot('sample');
+    assert.equal(browser.getTitle(), 'jQuery • TodoMVC');
+  });
 });
